4e6d3fbc7b73b8fbee744763301e53a573b7c9d1
Benoit Favre <benoit.favre@lif.univ-mrs.fr>
2014-10-02 16:28:26 +0200

    Udpate notification icon to follow recent style guidelines.

---
+++ CalDAVSyncAdapter/create-icons.sh
@@ -0,0 +1,6 @@
+inkscape calendar.svg -C -w 18 -h 18 -e res/drawable-ldpi/icon.png
+inkscape calendar.svg -C -w 24 -h 24 -e res/drawable-mdpi/icon.png
+inkscape calendar.svg -C -w 36 -h 36 -e res/drawable-hdpi/icon.png
+inkscape calendar.svg -C -w 48 -h 48 -e res/drawable-xhdpi/icon.png
+inkscape calendar.svg -C -w 90 -h 90 -e res/drawable-xxhdpi/icon.png
+inkscape calendar.svg -C -w 96 -h 96 -e res/drawable-xxxhdpi/icon.png
